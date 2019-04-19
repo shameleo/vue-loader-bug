8b4e1e8f82ef4be651b3283e1bf8a49ef28dcdf4
Pavel Khmelevskiy <Khmelevskiy@irc.beltelecom.by>
2019-04-19 14:33:09 +0300

    runtime icons

--- Test.vue
+++ Test.vue
@@ -2,9 +2,35 @@
 	<div>
 		<img src="~Icons/ok.png">
 		<img :src="'~Icons/ok.png'">
+
+		<div>========================</div>
+
+		<div>
+			<button @click="addIcon('ok')">ok icon</button>
+			<button @click="addIcon('fail')">fail icon</button>
+			<form @submit.prevent="addIcon(iconName)">
+				<input type="text" v-model="iconName">
+				<button type="submit">any icon (including subdir), without extension</button>
+			</form>
+			<div v-for="icon in icons">
+				<img :src="require(`Icons/${icon}.png`)">
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
-	export default {}
+	export default {
+		data() {
+			return {
+				icons: [],
+				iconName: ''
+			}
+		},
+		methods: {
+			addIcon(icon) {
+				this.icons.push(icon);
+			}
+		}
+	}
 </script>
